ab080b48257d27a3225d5fe6f7ee42daee45d3fa
German <haspenngames14@gmail.com>
2023-03-16 23:31:57 -0300

    proyects changes

--- src/components/Proyects/proyect.jsx
+++ src/components/Proyects/proyect.jsx
@@ -9,32 +9,61 @@ export function Proyect() {
         <div className="card">
           <img src="/hostel.png" alt="hostel"></img>
           <h2>Dinamita Hostel</h2>
-          <p className="card-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            velit, inventore ducimus reiciendis libero harum quam numquam ut
-            quod similique. Praesentium fugit magnam iste. Necessitatibus
-            consectetur repellendus quas eos animi.
+          <p className="infoHover">
+            {" "}
+            INFO <span>*hover*</span>{" "}
           </p>
+          <p className="card-description">
+            Single Page Application acerca de un hostel, es un proyecto grupal
+            destinado a dueños de Hostel que deseen tener su propia página web,
+            donde se puede haer gestion sobre las habitaciones. Por mi parte
+            aporte en el formulario de creacion y actividad de las habitaciones
+            para la subida de imágenes en el form
+          </p>
+          <a href="https://hostel-project.vercel.app/" target="_blank">
+            <span>Deploy</span>
+          </a>
+          <a href="https://github.com/kripto-c/HostelProject" target="_blank">
+            <span>Repositorio</span>
+          </a>
         </div>
         <div className="card">
           <img src="pokemons.png" alt="pokemons" />
           <h2>Pokemons Proyect</h2>
-          <p className="card-description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
-            eveniet ducimus rem, eum perferendis fugiat provident tempore! Quo
-            ipsa suscipit id autem eius sint voluptatum illum saepe iure
-            reprehenderit? Delectus!
+          <p className="infoHover">
+            {" "}
+            INFO <span>*hover*</span>
           </p>
+          <p className="card-description">
+            Proyecto individual para el bootcamp soy Henry con temática de
+            Pokemons, es una single-page application con barra de búsqueda,
+            formulario de creación de juegos, filtrado y ordenado de los mismos
+          </p>
+          <a
+            href="https://github.com/GermanSmigoski/Pokedex-proyect"
+            target="_blank"
+          >
+            <span>Repositorio</span>
+          </a>
         </div>
         <div className="card">
           <img src="videogames.png" alt="videogames" />
           <h2>Videogames Proyect</h2>
-          <p className="card-description">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nam
-            ducimus nulla esse labore. Eligendi unde laboriosam voluptatum
-            pariatur asperiores maiores esse ducimus libero in nesciunt! Est
-            perspiciatis hic dolor!
+          <p className="infoHover">
+            {" "}
+            INFO <span>*hover*</span>{" "}
           </p>
+          <p className="card-description">
+            Proyecto individual para el bootcamp soy Henry con temática de
+            videojuegos, es una single-page application con barra de búsqueda,
+            formulario de creación de juegos, filtrado y ordenado de los mismos
+          </p>
+          <a
+            href="https://github.com/GermanSmigoski/Videogames-proyect"
+            target="_blank"
+          >
+            <span>Repositorio</span>
+          </a>
         </div>
       </div>
     </div>
