4a17675c3687e379363a1fb4217fe268b0728c89
German <haspenngames14@gmail.com>
2023-03-16 23:01:53 -0300

    new styles and animations

--- src/components/About Me/about.jsx
+++ src/components/About Me/about.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./about.css";
 
 export function About() {
-  return <div className="aboutContainer" id="about">
-    <h2>Sobre Mi: </h2>
-    <p>
-      Mi nombre es German Smigoski tengo 20 años y acutalmente vivo en Rio Negro, Argentina.
-      Desde chico estuve interesado en la tecnologia y lo digital,
-    </p>
-  </div>;
+  return (
+    <div className="aboutContainer" id="about">
+      <h2>Sobre Mi: </h2>
+      <p>
+        Mi nombre es German Smigoski tengo 20 años y acutalmente vivo en Rio
+        Negro, Argentina. Desde chico estuve interesado en la tecnologia, me
+        considero una persona que logra lo que se propone y me gusta darle vida
+        a mis ideas. Soy fanatico de los detalles y pienso que los pequeños forman grandes cosas.
+      </p>
+    </div>
+  );
 }
