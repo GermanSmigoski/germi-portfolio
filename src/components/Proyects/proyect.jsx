3e91d043182bc3db15fe8ea71b558bf3ce316aad
German <haspenngames14@gmail.com>
2023-03-17 02:47:02 -0300

    changes in proyects not finished yet

--- src/components/Proyects/proyect.jsx
+++ src/components/Proyects/proyect.jsx
@@ -10,8 +10,7 @@ export function Proyect() {
           <img src="/hostel.png" alt="hostel"></img>
           <h2>Dinamita Hostel</h2>
           <p className="infoHover">
-            {" "}
-            INFO <span>*hover*</span>{" "}
+            INFO <span>*hover*</span>
           </p>
           <p className="card-description">
             Single Page Application acerca de un hostel, es un proyecto grupal
@@ -20,18 +19,27 @@ export function Proyect() {
             aporte en el formulario de creacion y actividad de las habitaciones
             para la subida de imágenes en el form
           </p>
-          <a href="https://hostel-project.vercel.app/" target="_blank">
-            <span>Deploy</span>
-          </a>
-          <a href="https://github.com/kripto-c/HostelProject" target="_blank">
-            <span>Repositorio</span>
-          </a>
+          <div className="skillsProyect">
+            <span>React</span>
+            <span>Redux</span>
+            <span>Boostrap</span>
+            <span>Node.js</span>
+            <span>Express</span>
+            <span>PostgreSQL</span>
+          </div>
+          <div className="cardLink">
+            <a href="https://hostel-project.vercel.app/" target="_blank">
+              <span>Deploy</span>
+            </a>
+            <a href="https://github.com/kripto-c/HostelProject" target="_blank">
+              <span>Repositorio</span>
+            </a>
+          </div>
         </div>
         <div className="card">
           <img src="pokemons.png" alt="pokemons" />
           <h2>Pokemons Proyect</h2>
           <p className="infoHover">
-            {" "}
             INFO <span>*hover*</span>
           </p>
           <p className="card-description">
@@ -39,31 +47,34 @@ export function Proyect() {
             Pokemons, es una single-page application con barra de búsqueda,
             formulario de creación de juegos, filtrado y ordenado de los mismos
           </p>
-          <a
-            href="https://github.com/GermanSmigoski/Pokedex-proyect"
-            target="_blank"
-          >
-            <span>Repositorio</span>
-          </a>
+          <div className="cardLink">
+            <a
+              href="https://github.com/GermanSmigoski/Pokedex-proyect"
+              target="_blank"
+            >
+              <span>Repositorio</span>
+            </a>
+          </div>
         </div>
         <div className="card">
           <img src="videogames.png" alt="videogames" />
           <h2>Videogames Proyect</h2>
           <p className="infoHover">
-            {" "}
-            INFO <span>*hover*</span>{" "}
+            INFO <span>*hover*</span>
           </p>
           <p className="card-description">
             Proyecto individual para el bootcamp soy Henry con temática de
             videojuegos, es una single-page application con barra de búsqueda,
             formulario de creación de juegos, filtrado y ordenado de los mismos
           </p>
-          <a
-            href="https://github.com/GermanSmigoski/Videogames-proyect"
-            target="_blank"
-          >
-            <span>Repositorio</span>
-          </a>
+          <div className="cardLink">
+            <a
+              href="https://github.com/GermanSmigoski/Videogames-proyect"
+              target="_blank"
+            >
+              <span>Repositorio</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
