de99afea4818c3579e4d44c6390464fe1ed5ffba
German <haspenngames14@gmail.com>
2023-03-30 13:53:27 -0300

    css changes, info json, proyects

---
+++ src/components/Proyects/info.js
@@ -0,0 +1,34 @@
+export const infoProyects = {
+  data: [
+    {
+      id: '1',
+      img: "hostel.png",
+      nombre: "Dinamita Hostel",
+      descripcion:
+        "Single Page Application acerca de un hostel, es un proyecto grupaldestinado a dueños de Hostel que deseen tener su propia página webdonde se puede haer gestion sobre las habitaciones. Por mi parteaporte en el formulario de creacion y actividad de las habitaciones para la subida de imágenes en el form",
+      skills: ["React", "Redux", "Boostrap", "Node.js", "Express"],
+      deploy: "https://hostel-project.vercel.app/",
+      repositorio: "https://github.com/kripto-c/HostelProject",
+    },
+    {
+      id: '2',
+      img: "pokemons.png",
+      nombre: "Pokedex Proyect",
+      descripcion:
+        "Proyecto individual para el bootcamp soy Henry con temática de Pokemons, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
+      skills: ["React", "Redux", "Boostrap", "Node.js", "Express"],
+      deploy: "",
+      repositorio: "https://github.com/GermanSmigoski/Pokedex-proyect",
+    },
+    {
+      id: '3',
+      img: "videogames.png",
+      nombre: "Videogames Proyect",
+      descripcion:
+        "Proyecto individual para el bootcamp soy Henry con temática de videojuegos, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
+      skills: ["React", "Redux", "Boostrap", "Node.js", "Express"],
+      deploy: "",
+      repositorio: "https://github.com/GermanSmigoski/Videogames-proyect",
+    },
+  ],
+};
--- src/components/Proyects/proyect.jsx
+++ src/components/Proyects/proyect.jsx
@@ -1,100 +1,36 @@
 import React from "react";
 import "./proyect.css";
+import { infoProyects } from "./info";
 
 export function Proyect() {
   return (
     <div className="proyects" id="proyects">
       <h1>Proyects</h1>
       <div className="cards">
-        <div className="card">
-          <img src="/hostel.png" alt="hostel"></img>
-          <h2>Dinamita Hostel</h2>
-          <p className="infoHover">
-            INFO <span>*hover*</span>
-          </p>
-          <p className="card-description">
-            Single Page Application acerca de un hostel, es un proyecto grupal
-            destinado a dueños de Hostel que deseen tener su propia página web,
-            donde se puede haer gestion sobre las habitaciones. Por mi parte
-            aporte en el formulario de creacion y actividad de las habitaciones
-            para la subida de imágenes en el form
-          </p>
-          <div className="skillsProyect">
-            <span>React</span>
-            <span>Redux</span>
-            <span>Boostrap</span>
-            <span>Node.js</span>
-            <span>Express</span>
+        {infoProyects.data.map((proyect) => (
+          <div key={proyect.id} className="card">
+            <img src={proyect.img} alt={proyect.name}></img>
+            <div className="card-info">
+              <h2>{proyect.nombre}</h2>
+              <p>{proyect.descripcion}</p>
+              <div className="card-skills">
+                {proyect.skills.map((skill) => (
+                  <p key={skill}>{skill}</p>
+                ))}
+              </div>
+              <div className="card-links">
+                {proyect.deploy && (
+                  <a href={proyect.deploy} target="_blank">
+                    <p>Deploy:{proyect.deploy}</p>
+                  </a>
+                )}
+                <a href={proyect.repositorio} target="_blank">
+                  <p>Repositiorio: {proyect.repositorio}</p>
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="cardLink">
-            <a href="https://hostel-project.vercel.app/" target="_blank">
-              <span>Deploy</span>
-            </a>
-            <a href="https://github.com/kripto-c/HostelProject" target="_blank">
-              <span>Repositorio</span>
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <img src="pokemons.png" alt="pokemons" />
-          <h2>Pokemons Proyect</h2>
-          <p className="infoHover">
-            INFO <span>*hover*</span>
-          </p>
-          <p className="card-description">
-            Proyecto individual para el bootcamp soy Henry con temática de
-            Pokemons, es una single-page application con barra de búsqueda,
-            formulario de creación de juegos, filtrado y ordenado de los mismos
-          </p>
-          <div className="skillsProyect">
-            <span>React</span>
-            <span>Redux</span>
-            <span>Boostrap</span>
-            <span>Node.js</span>
-            <span>Express</span>
-          </div>
-          <div className="cardLink">
-            <a href="">
-              <span>Deploy</span>
-            </a>
-            <a
-              href="https://github.com/GermanSmigoski/Pokedex-proyect"
-              target="_blank"
-            >
-              <span>Repositorio</span>
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <img src="videogames.png" alt="videogames" />
-          <h2>Videogames Proyect</h2>
-          <p className="infoHover">
-            INFO <span>*hover*</span>
-          </p>
-          <p className="card-description">
-            Proyecto individual para el bootcamp soy Henry con temática de
-            videojuegos, es una single-page application con barra de búsqueda,
-            formulario de creación de juegos, filtrado y ordenado de los mismos
-          </p>
-          <div className="skillsProyect">
-            <span>React</span>
-            <span>Redux</span>
-            <span>Boostrap</span>
-            <span>Node.js</span>
-            <span>Express</span>
-          </div>
-          <div className="cardLink">
-            <a href="">
-              <span>Deploy</span>
-            </a>
-            <a
-              href="https://github.com/GermanSmigoski/Videogames-proyect"
-              target="_blank"
-            >
-              <span>Repositorio</span>
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
