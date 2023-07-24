d29f47051d7927e0d8c6336829edd186b8c3ff5f
German <haspenngames14@gmail.com>
2023-07-24 15:57:34 -0300

    projects changes

--- src/components/Projects/info.js
+++ src/components/Projects/info.js
@@ -2,7 +2,7 @@ export const infoProjects = {
   es: [
     {
       id: "1",
-      img: "dinamita.webp",
+      image: "dinamita.webp",
       nombre: "Dinamita Hostel",
       descripcion:
         "Single Page Application acerca de un hostel, es un proyecto grupaldestinado a dueños de Hostel que deseen tener su propia página webdonde se puede haer gestion sobre las habitaciones. Por mi parteaporte en el formulario de creacion y actividad de las habitaciones para la subida de imágenes en el form",
@@ -12,8 +12,8 @@ export const infoProjects = {
     },
     {
       id: "2",
-      img: "pokemons.webp",
-      nombre: "Pokedex Proyect",
+      image: "pokemons.webp",
+      nombre: "Pokedex Project",
       descripcion:
         "Proyecto individual para el bootcamp soy Henry con temática de Pokemons, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
       skills: ["React", "Redux", "Boostrap", "Node.js", "Express"],
@@ -22,17 +22,17 @@ export const infoProjects = {
     },
     {
       id: "3",
-      img: "videogames.webp",
-      nombre: "Videogames Proyect",
+      image: "videogames.webp",
+      nombre: "Videogames Project",
       descripcion:
         "Proyecto individual para el bootcamp soy Henry con temática de videojuegos, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
-      skills: ["React", "Redux", "Boostrap", "Node.js", "Express",],
+      skills: ["React", "Redux", "Boostrap", "Node.js", "Express"],
       deploy: "",
       repositorio: "https://github.com/GermanSmigoski/Videogames-proyect",
     },
     {
       id: "4",
-      img: "valorant.webp",
+      image: "valorant.webp",
       nombre: "Valorant info",
       descripcion:
         "Single Page Application que enseña informacion acerca del juego valorant el cual lo utilice para practicar estilos mas que nada y las llamadas a la api.",
@@ -44,7 +44,7 @@ export const infoProjects = {
   en: [
     {
       id: "1",
-      img: "dinamita.webp",
+      image: "dinamita.webp",
       nombre: "Dinamita Hostel",
       descripcion:
         "Single Page Application acerca de un hostel, es un proyecto grupaldestinado a dueños de Hostel que deseen tener su propia página webdonde se puede haer gestion sobre las habitaciones. Por mi parteaporte en el formulario de creacion y actividad de las habitaciones para la subida de imágenes en el form",
@@ -54,7 +54,7 @@ export const infoProjects = {
     },
     {
       id: "2",
-      img: "pokemons.webp",
+      image: "pokemons.webp",
       nombre: "Pokedex Proyect",
       descripcion:
         "Proyecto individual para el bootcamp soy Henry con temática de Pokemons, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
@@ -64,7 +64,7 @@ export const infoProjects = {
     },
     {
       id: "3",
-      img: "videogames.webp",
+      image: "videogames.webp",
       nombre: "Videogames Proyect",
       descripcion:
         "Proyecto individual para el bootcamp soy Henry con temática de videojuegos, es una single-page application con barra de búsqueda, formulario de creación de juegos, filtrado y ordenado de los mismos",
@@ -74,7 +74,7 @@ export const infoProjects = {
     },
     {
       id: "4",
-      img: "valorant.webp",
+      image: "valorant.webp",
       nombre: "Valorant info",
       descripcion:
         "Single Page Application que enseña informacion acerca del juego valorant el cual lo utilice para practicar estilos mas que nada y las llamadas a la api.",
--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -1,59 +1,56 @@
 import React, { useState } from "react";
+import { FaInfoCircle } from "react-icons/fa"; // Importa el icono de información
 import "./project.css";
 import { infoProjects } from "./info";
 
 export function Project() {
-  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+  const [showDescription, setShowDescription] = useState(false);
 
-  const nextProject = () => {
-    setCurrentProjectIndex((prevIndex) =>
-      prevIndex === infoProjects.es.length - 1 ? 0 : prevIndex + 1
-    );
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
   };
-
-  const prevProject = () => {
-    setCurrentProjectIndex((prevIndex) =>
-      prevIndex === 0 ? infoProjects.es.length - 1 : prevIndex - 1
-    );
-  };
-
-  // Move the currentProject declaration here after setting the index
-  const currentProject = infoProjects.es[currentProjectIndex];
 
   return (
-    <section className="proyects" id="proyects">
-      <h1>Proyects</h1>
-      <div className="slider-container">
-        <button onClick={prevProject}>Previous</button>
-        <div className="card">
-          <img src={currentProject.img} alt={currentProject.name} />
-          <div className="card-info">
-            <h2>{currentProject.nombre}</h2>
-            <p>{currentProject.descripcion}</p>
-            <div className="card-skills">
-              {currentProject.skills.map((skill) => (
-                <span key={skill}>{skill}</span>
-              ))}
-            </div>
-            <div className="card-links">
-              {currentProject.deploy && (
-                <p>
-                  Deploy:
-                  <a href={currentProject.deploy} target="_blank">
-                    <span>{currentProject.deploy}</span>
-                  </a>
-                </p>
+    <section className="projects" id="projects">
+      <h1>Projects:</h1>
+      <div className="project-grid">
+        {infoProjects &&
+          infoProjects.es.map((project) => (
+            <div key={project.id} className="project-card">
+              <img
+                src={project.image}
+                alt={`${project.name} image`}
+                className={`project-image ${showDescription ? "hidden" : ""}`}
+              />
+              <h3 className="project-title">{project.nombre}</h3>
+              <button onClick={toggleDescription} className="info-button">
+                <FaInfoCircle />
+              </button>
+              {showDescription && (
+                <p className="project-description">{project.descripcion}</p>
               )}
-              <p>
-                Repositiorio:
-                <a href={currentProject.repositorio} target="_blank">
-                  <span>{currentProject.repositorio}</span>
+              <div
+                className={`project-links ${showDescription ? "hidden" : ""}`}
+              >
+                <a
+                  href={project.repositorio}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Ver Repositorio
                 </a>
-              </p>
+                {project.deploy && (
+                  <a
+                    href={project.deploy}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ver Despliegue
+                  </a>
+                )}
+              </div>
             </div>
-          </div>
-        </div>
-        <button onClick={nextProject}>Next</button>
+          ))}
       </div>
     </section>
   );
